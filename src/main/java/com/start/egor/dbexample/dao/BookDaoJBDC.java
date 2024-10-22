478249ebe1f4e578e48b5a39d16b64fb3b9288c1
stud <stud@it224-14.almetpt.local>
2024-10-22 13:22:51 +0300

    add model, dao

---
+++ src/main/java/com/start/egor/dbexample/dao/BookDaoJBDC.java
@@ -0,0 +1,35 @@
+package com.start.egor.dbexample.dao;
+
+import com.start.egor.dbexample.model.Book;
+
+import java.sql.*;
+
+public class BookDaoJBDC {
+    public Book findBookById(Integer bookId){
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:postgresql://localhost:5433/local_db2", "postgres", "12345")) {
+            if (connection != null){
+                System.out.println("Мы подключились к БД");
+            } else{
+                System.out.println("БД недоступна.");
+            }
+
+            String select = "select * from books where id = ?";
+            PreparedStatement selectQuery = connection.prepareStatement(select);
+            selectQuery.setInt(1, bookId);
+            ResultSet resultSet = selectQuery.executeQuery();
+
+            while (resultSet.next()){
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setTitle(resultSet.getString("title"));
+                book.setDateAdded(resultSet.getDate("date_added"));
+                System.out.println(book);
+            }
+        } catch (SQLException ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return null;
+    }
+}
