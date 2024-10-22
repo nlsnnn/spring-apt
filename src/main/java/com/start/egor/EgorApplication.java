2a393284f0f38fb4f846d19d4b319049b594e6c1
stud <stud@it224-14.almetpt.local>
2024-10-22 11:48:10 +0300

    init commit

---
+++ src/main/java/com/start/egor/EgorApplication.java
@@ -0,0 +1,13 @@
+package com.start.egor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/start/egor/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.start.egor;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EgorApplication.class);
+	}
+
+}
