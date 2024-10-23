9f5ab4e13cb5feaff11aeb48a3adf2c6f994fd9d
stud <stud@it231-14.almetpt.local>
2024-10-23 13:22:53 +0300

    print all

--- src/main/java/com/start/egor/EgorApplication.java
+++ src/main/java/com/start/egor/EgorApplication.java
@@ -1,14 +1,27 @@
 package com.start.egor;
 
 import com.start.egor.dbexample.dao.BookDAOBean;
+import com.start.egor.dbexample.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class EgorApplication implements CommandLineRunner {
 
 	private BookDAOBean bookDAOBean;
+
+	@Autowired
+	public void setBookDAOBean(BookDAOBean bookDAOBean) {
+		this.bookDAOBean = bookDAOBean;
+	}
+
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
 
 	public EgorApplication(BookDAOBean bookDAOBean){
 		this.bookDAOBean = bookDAOBean;
@@ -20,7 +33,16 @@ public class EgorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception{
-		bookDAOBean.findBookById(3);
+//		bookDAOBean.findBookById(3);
+		List<Book> bookList = jdbcTemplate.query("select * from books",
+				((rs, rowNum) -> new Book(
+						rs.getInt("id"),
+						rs.getString("title"),
+						rs.getString("author"),
+						rs.getDate("date_added")
+				)));
+		bookList.forEach(System.out::println);
 	}
+
 
 }
